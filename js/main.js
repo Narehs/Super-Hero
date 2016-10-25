47afbe0ef67871fac0826a0df7093b7f6fd0b687
Nareh <Nareh>
2016-10-26 00:04:52 +0400

    adding

---
+++ js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+	$('.flexslider').flexslider({
+    animation: "slide",
+	useCSS: false,
+	controlNav:false,
+	after: function (slider) {  
+		if (!slider.playing) {
+            setTimeout(function(){
+				slider.play();	
+			},3000);
+		}
+    }
+  });
+});
+
+
+
+  $(document).ready(function(){
+  	$("#tabs").tabs();
+});
+
+
+ $(document).ready(function(){
+ 	$("#SliderSingle").slider({ 
+ 		from: 40, 
+ 		to: 260, 
+ 		step: 10, 
+ 		round: 1, 
+ 		format: { format: '##.0', locale: 'de' }, 
+ 		dimension: '&nbsp;sq.m', 
+ 		skin: "round" });
+ 	});
+
+
+  $(document).ready(function () {
+            $('.fancybox-thumbs').fancybox({
+//                prevEffect: 'none',
+//                nextEffect: 'none',
+                closeBtn: false,
+                arrows: false,
+                nextClick: true,
+
+                helpers: {
+                    thumbs: {
+                        width: 50,
+                        height: 50
+                    }
+                }
+            });
+        });
